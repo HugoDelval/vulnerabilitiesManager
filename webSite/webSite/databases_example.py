6b471b029c8aaf2e3036dcd63ccc395b123b51e3
HugoDelval <hugo.delval@insa-lyon.fr>
2015-07-31 10:03:07 +0200

    databases example

---
+++ webSite/webSite/databases_example.py
@@ -0,0 +1,13 @@
+
+def getDatabaseConfig():
+	DATABASES = {
+		'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'kmbdd',
+		'HOST': 'localhost',
+		'PASSWORD': '',
+		'PORT': '',
+		'USER': 'root',
+		}
+	}
+	return DATABASES
