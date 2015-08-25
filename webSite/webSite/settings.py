3c7310249ceccc6e476aade6f271c99d4f6b9265
HugoDelval <hugo.delval@insa-lyon.fr>
2015-08-25 11:33:33 +0200

    loggin -- settings.py

--- webSite/webSite/settings.py
+++ webSite/webSite/settings.py
@@ -110,8 +110,3 @@ LOGIN_URL = '/user/login/'
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
-import logging
-
-logger = logging.getLogger('django_auth_ldap')
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.DEBUG)
