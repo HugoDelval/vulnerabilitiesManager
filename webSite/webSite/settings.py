b7008b22444efd1623abc0275000de19f86e15e1
audit <audit@localhost.localdomain>
2015-08-28 16:24:33 +0200

    retablissement des settings de prod

--- webSite/webSite/settings.py
+++ webSite/webSite/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kqsjd!:,sdfn_75467çu__è"_è566qfdqzGZRG628/*+QEF'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,7 +101,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/hdl/KM/vulnerabilitiesManager/webSite/static/'
-
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+os.environ['wsgi.url_scheme'] = 'https'
+SECURE_HSTS_SECONDS = True
+SECURE_SSL_REDIRECT = True
 GRAPPELLI_ADMIN_TITLE = 'KMAmossys'
 
 ADMINS = (
